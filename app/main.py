1a2e7a85b9b4e31b1ebce891efae897d9d541994
Hryhoriy <hryhoriy.bilous@gmail.com>
2024-06-27 15:50:27 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 import time
-
+import asyncio
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-def main() -> None:
+async def main() -> None:
     # create an IOT service
     service = IOTService()
 
@@ -13,15 +13,19 @@ def main() -> None:
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_id = service.register_device(hue_light)
-    speaker_id = service.register_device(speaker)
-    toilet_id = service.register_device(toilet)
+    hue_light_id = await service.register_device(hue_light)
+    speaker_id = await service.register_device(speaker)
+    toilet_id = await service.register_device(toilet)
 
     # create a few programs
     wake_up_program = [
         Message(hue_light_id, MessageType.SWITCH_ON),
         Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+        Message(
+            speaker_id,
+            MessageType.PLAY_SONG,
+            "Rick Astley - Never Gonna Give You Up"
+        ),
     ]
 
     sleep_program = [
@@ -32,13 +36,35 @@ def main() -> None:
     ]
 
     # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    await service.run_sequence(
+        service.send_msg(wake_up_program[0]),
+        service.run_parallel(
+            service.send_msg(wake_up_program[1]),
+            service.send_msg(wake_up_program[2]),
+        ),
+    )
+
+    await service.run_sequence(
+        service.send_msg(sleep_program[0]),
+        service.run_parallel(
+            service.send_msg(sleep_program[1]),
+            service.run_sequence(
+                service.send_msg(sleep_program[2]),
+                service.send_msg(sleep_program[3]),
+            ),
+        ),
+    )
+
+    await service.run_parallel(
+        service.unregister_device(hue_light_id),
+        service.unregister_device(speaker_id),
+        service.unregister_device(toilet_id),
+    )
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
